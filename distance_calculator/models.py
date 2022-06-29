b8b395be51aff14c5104306c7d426ec943bfc697
rudeuke <bartekqql@gmail.com>
2022-06-29 13:00:34 +0200

    Created request model

--- distance_calculator/models.py
+++ distance_calculator/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Request(models.Model):
+    request_id = models.IntegerField(primary_key=True)
+    start_timestamp = models.DateTimeField()
+    end_timestamp = models.DateTimeField()
