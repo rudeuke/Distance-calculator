bfad29f456394c13a51fc144ff954cb37fa3e70d
BK <bartekqql@gmail.com>
2023-06-22 19:28:45 +0200

    Add loading animation

--- stava_geodistance/static/ajax.js
+++ stava_geodistance/static/ajax.js
@@ -3,21 +3,26 @@ $(document).ready(function () {
         event.preventDefault();
         var formData = $(this).serialize();
 
+        document.querySelector('#submit-button').innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
+        document.querySelector('#submit-button').disabled = true;
+
         $.ajax({
             type: 'POST',
             url: `${document.location.origin}`,
             data: formData,
+            
+        }).done(function (response) {
+            console.log(response);
+            $('#results').addClass("bg-light border border-secondary rounded");
+            $('#total-distance').text("Total distance: " + response.total_distance + " km");
+            $('#calculation-time').text("Calculation time: " + response.calculation_time + " s");
 
-            success: function (response) {
-                console.log(response);
-                $('#results').addClass("bg-light border border-secondary rounded");
-                $('#total-distance').text("Total distance: " + response.total_distance + " km");
-                $('#calculation-time').text("Calculation time: " + response.calculation_time + " s");
-            },
+        }).fail(function () {
+            alert("Error contacting API");
 
-            error: function () {
-                alert("Error contacting API");
-            },
+        }).always(function () {
+            document.querySelector('#submit-button').innerHTML = '<span><h5>Calculate distance</h5></span>';
+            document.querySelector('#submit-button').disabled = false;
         });
     });
 });
